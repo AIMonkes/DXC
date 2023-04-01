035934195096bed779ee51f9dcd93955b5be5c56
Ivan46464 <113828394+Ivan46464@users.noreply.github.com>
2023-04-02 00:49:37 +0300

    Add files via upload

---
+++ mysite/vsetaq/forms.py
@@ -0,0 +1,37 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Income, Expenses, Goals
+
+
+class IncomeForm(ModelForm):
+    class Meta:
+        model = Income
+        fields = '__all__'
+        exclude = ['user']
+        widgets = {
+            'income': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class ExpensesForm(ModelForm):
+    class Meta:
+        model = Expenses
+        fields = '__all__'
+        exclude = ['user']
+        widgets = {
+            'expenses': forms.TextInput(attrs={'class': 'form-control'}),
+            'categories': forms.TextInput(attrs={'class': 'form-control'})
+
+        }
+
+
+class GoalsForm(ModelForm):
+
+    class Meta:
+        model = Goals
+        fields = '__all__'
+        exclude = ['user']
+        widgets = {
+            'save_money': forms.TextInput(attrs={'class': 'form-control'}),
+            'goal': forms.TextInput(attrs={'class': 'form-control'})
+        }
